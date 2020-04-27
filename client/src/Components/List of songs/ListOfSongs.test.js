2a7d0f3c564d635dd8b0cf71f543ae8ef5622801
gsolde <gsolde@gmail.com>
2020-04-27 19:28:03 +0200

    test: create listOfSongs file and mocked data from wikipediaService

---
+++ client/src/Components/List of songs/ListOfSongs.test.js
@@ -0,0 +1,30 @@
+
+
+const mockedUrls = [ // res of GetWikiUrls
+  "https://en.wikipedia.org/wiki/Watchmen",
+  "https://en.wikipedia.org/wiki/Watchmen_(TV_series)",
+  "https://en.wikipedia.org/wiki/Watchmen_(film)",
+  "https://en.wikipedia.org/wiki/Watchman_(law_enforcement)",
+  "https://en.wikipedia.org/wiki/Watchmen:_The_End_Is_Nigh",
+  "https://en.wikipedia.org/wiki/Watchmen_Recording_Studios",
+  "https://en.wikipedia.org/wiki/Watchmen:_Music_from_the_Motion_Picture",
+  "https://en.wikipedia.org/wiki/Watchmen:_Motion_Comic",
+  "https://en.wikipedia.org/wiki/Watchmen_on_the_Walls",
+  "https://en.wikipedia.org/wiki/Watchman_Fellowship",
+  "https://en.wikipedia.org/wiki/Watchmen_Drum_and_Bugle_Corps"
+]
+
+const mockedSongs = [ // res of GetTitles(url)
+  "Desolation Row",
+  "Unforgettable",
+  "The Times They Are a-Changin'",
+  "The Sound of Silence",
+  "Me and Bobby McGee",
+  "I'm Your Boogie Man",
+  "You're My Thrill",
+  "Pruit Igoe",
+  "Hallelujah",
+  "All Along the Watchtower",
+  "Ride of the Valkyries",
+  "Pirate Jenny"
+]
