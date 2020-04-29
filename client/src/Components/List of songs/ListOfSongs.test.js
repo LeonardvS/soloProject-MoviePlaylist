f4ae38784e94f754ed7b9941387377572f08e2c5
gsolde <gsolde@gmail.com>
2020-04-29 13:30:23 +0200

    refactor: get rid of unnecessary imports

--- client/src/Components/List of songs/ListOfSongs.test.js
+++ client/src/Components/List of songs/ListOfSongs.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Enzyme from 'enzyme';
 import { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { render, cleanup, act, waitForElement, waitForElementToBeRemoved, screen } from '@testing-library/react';
+import { render, cleanup, waitForElementToBeRemoved } from '@testing-library/react';
 import { getTrackList } from '../../services/wikipediaService'
 import ListOfSongs from './ListOfSongs'
 
